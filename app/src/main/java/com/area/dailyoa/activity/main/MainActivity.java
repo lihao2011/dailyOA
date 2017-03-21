81f2f30fa51a40409abfb83b5b866f68a8bfe4f6
lihao2011 <lihao_2011@163.com>
2017-03-21 18:48:16 +0800

    Fix：底部Tab添加

--- app/src/main/java/com/area/dailyoa/activity/main/MainActivity.java
+++ app/src/main/java/com/area/dailyoa/activity/main/MainActivity.java
@@ -20,33 +20,33 @@ public class MainActivity extends BaseActivity {//AppCompatActivity
     @Bind(R.id.main_viewpager)
     ViewPager main_viewpager;
     //消息
-    @Bind(R.id.tab_img_message)
-    ImageView tab_img_message;
-    @Bind(R.id.tab_text_message)
-    TextView tab_text_message;
-    @Bind(R.id.message_num)
-    DragPointView message_num;
-    //项目部
-    @Bind(R.id.tab_img_project)
-    ImageView tab_img_project;
-    @Bind(R.id.tab_text_project)
-    TextView tab_text_project;
-    @Bind(R.id.project_red)
-    ImageView project_red;
-    //联系人
-    @Bind(R.id.tab_img_contacts)
-    ImageView tab_img_contacts;
-    @Bind(R.id.tab_text_contacts)
-    TextView tab_text_contacts;
-    @Bind(R.id.contacts_num)
-    DragPointView contacts_num;
-    //我的
-    @Bind(R.id.tab_img_me)
-    ImageView tab_img_me;
-    @Bind(R.id.tab_text_me)
-    TextView tab_text_me;
-    @Bind(R.id.mine_red)
-    ImageView mine_red;
+//    @Bind(R.id.tab_img_message)
+//    ImageView tab_img_message;
+//    @Bind(R.id.tab_text_message)
+//    TextView tab_text_message;
+//    @Bind(R.id.message_num)
+//    DragPointView message_num;
+//    //项目部
+//    @Bind(R.id.tab_img_project)
+//    ImageView tab_img_project;
+//    @Bind(R.id.tab_text_project)
+//    TextView tab_text_project;
+//    @Bind(R.id.project_red)
+//    ImageView project_red;
+//    //联系人
+//    @Bind(R.id.tab_img_contacts)
+//    ImageView tab_img_contacts;
+//    @Bind(R.id.tab_text_contacts)
+//    TextView tab_text_contacts;
+//    @Bind(R.id.contacts_num)
+//    DragPointView contacts_num;
+//    //我的
+//    @Bind(R.id.tab_img_me)
+//    ImageView tab_img_me;
+//    @Bind(R.id.tab_text_me)
+//    TextView tab_text_me;
+//    @Bind(R.id.mine_red)
+//    ImageView mine_red;
 
     @Override
     protected int getLayoutId() {
@@ -59,13 +59,6 @@ public class MainActivity extends BaseActivity {//AppCompatActivity
     }
 
     private void changeTextViewColor() {
-        tab_img_message.setBackgroundDrawable(getResources().getDrawable(R.mipmap.ic_message_nomal));
-        tab_img_project.setBackgroundDrawable(getResources().getDrawable(R.mipmap.ic_project_nomal));
-        tab_img_contacts.setBackgroundDrawable(getResources().getDrawable(R.mipmap.ic_contact_normal));
-        tab_img_me.setBackgroundDrawable(getResources().getDrawable(R.mipmap.ic_mine_normal));
-        tab_text_message.setTextColor(Color.parseColor("#abadbb"));
-        tab_text_project.setTextColor(Color.parseColor("#abadbb"));
-        tab_text_contacts.setTextColor(Color.parseColor("#abadbb"));
-        tab_text_me.setTextColor(Color.parseColor("#abadbb"));
+
     }
 }
