b9597b0550a83b1f655899de86efeec8b70a4850
lihao2011 <lihao_2011@163.com>
2017-04-14 10:37:50 +0800

    Fix：tab修改

--- app/src/main/java/com/area/dailyoa/activity/main/MainActivity.java
+++ app/src/main/java/com/area/dailyoa/activity/main/MainActivity.java
@@ -1,43 +1,50 @@
 package com.area.dailyoa.activity.main;
 
+import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.area.dailyoa.R;
 import com.area.dailyoa.activity.base.BaseActivity;
 
+import butterknife.Bind;
 
 
 public class MainActivity extends BaseActivity {//AppCompatActivity
 
 
-//    @Bind(R.id.main_viewpager)
-//    ViewPager main_viewpager;
+    //viewpager
+    @Bind(R.id.main_viewpager)
+    ViewPager main_viewpager;
     //消息
-//    @Bind(R.id.tab_img_message)
-//    ImageView tab_img_message;
-//    @Bind(R.id.tab_text_message)
-//    TextView tab_text_message;
-//    @Bind(R.id.message_num)
-//    DragPointView message_num;
-//    //项目部
-//    @Bind(R.id.tab_img_project)
-//    ImageView tab_img_project;
-//    @Bind(R.id.tab_text_project)
-//    TextView tab_text_project;
-//    @Bind(R.id.project_red)
-//    ImageView project_red;
-//    //联系人
-//    @Bind(R.id.tab_img_contacts)
-//    ImageView tab_img_contacts;
-//    @Bind(R.id.tab_text_contacts)
-//    TextView tab_text_contacts;
-//    @Bind(R.id.contacts_num)
-//    DragPointView contacts_num;
-//    //我的
-//    @Bind(R.id.tab_img_me)
-//    ImageView tab_img_me;
-//    @Bind(R.id.tab_text_me)
-//    TextView tab_text_me;
-//    @Bind(R.id.mine_red)
-//    ImageView mine_red;
+    @Bind(R.id.rlyt_message)
+    RelativeLayout rlyt_message;//Tab
+    @Bind(R.id.tab_message)
+    TextView tab_message;//文字图片
+    @Bind(R.id.red_message)
+    TextView red_message;//红点
+    //项目部
+    @Bind(R.id.rlyt_workgroup)
+    RelativeLayout rlyt_workgroup;//Tab
+    @Bind(R.id.tab_workgroup)
+    TextView tab_workgroup;//文字图片
+    @Bind(R.id.red_workgroup)
+    ImageView red_workgroup;//红点
+    //联系人
+    @Bind(R.id.rlyt_contacts)
+    RelativeLayout rlyt_contacts;//Tab
+    @Bind(R.id.tab_contacts)
+    TextView tab_contacts;//文字图片
+    @Bind(R.id.red_contacts)
+    TextView red_contacts;//红点
+    //我的
+    @Bind(R.id.rlyt_mine)
+    RelativeLayout rlyt_mine;//Tab
+    @Bind(R.id.tab_mine)
+    TextView tab_mine;//文字图片
+    @Bind(R.id.red_mine)
+    ImageView red_mine;//红点
 
     @Override
     protected int getLayoutId() {
